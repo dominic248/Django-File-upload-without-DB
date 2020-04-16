6e1c02ebd1d82bd78d8f07b010aa69e7da98cfea
Dominic Michael Silveira <dms24081999@gmail.com>
2020-04-17 01:59:03 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# File upload and save to folder without database
+
+## Installing virtualenv:
+Windows:
+```
+pip install virtualenv
+```
+
+Linux:
+```
+sudo apt install virtualenv
+```
+## Creating virtual environment via virtualenv:
+Windows:
+```
+virtualenv -p python env
+```
+
+Linux:
+```
+virtualenv -p python3 env
+```
+
+## Activate virtual environment:
+Windows:
+```
+env\Scripts\activate
+```
+
+Linux:
+```
+source env/bin/activate
+```
+
+## Browse into source folder: 
+```
+cd src
+```
+
+## Requirements Installation:
+Windows:
+```
+pip install -r requirements.txt
+```
+
+Linux:
+```
+pip3 install -r requirements.txt
+```
+
+## Run project:
+Windows:
+```
+python manage.py runserver
+```
+
+Linux:
+```
+python3 manage.py runserver
+```
+
+
---
+++ src/mysite/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect,HttpResponse
+from django.urls import reverse
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+import time
+
+
+def return_page(request): 
+	return HttpResponse("Success") 
+
+def home_page(request):
+    if request.method == 'POST':
+        # print(request.POST)
+        # print(request.FILES)
+        # print(request.FILES['img'],request.FILES['img'].name,type(request.FILES['img']))
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        # print(timestr)
+        data = request.FILES['img']
+        path = default_storage.save('tmp/image-'+timestr+'.jpg', ContentFile(data.read()))
+        return HttpResponseRedirect(reverse('return') )
+    return render(request, 'home.html')
